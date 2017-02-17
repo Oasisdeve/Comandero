32f822d4acb69a1124a886436906451f41c005ea
oasisdeve <developoasis@gmail.com>
2017-02-17 12:45:01 -0500

    otro

---
+++ app/src/main/java/oasispv/pv/variables.java
@@ -0,0 +1,36 @@
+package oasispv.pv;
+
+/**
+ * Created by Usuario on 28/11/2016.
+ */
+
+public class variables {
+
+    public static String ip;
+    public static String cn;
+    public static String un;
+    public static String pw;
+    public static String movi;
+    public static String fase;
+    public static String mesero;
+    public static String movi_desc;
+    public static String mesa;
+    public static int sesion;
+    public static int comensal;
+    public static int tiempo;
+    public static String cmd;
+    public static int max_id;
+    public static String modipv;
+    public static String mandatory;
+    public static String prdesc;
+    public static String mesero_mesa;
+    public static String comensal_name;
+    public static String rva;
+    public static String habi;
+    public static int mesa_pax;
+    public static int erroroc;
+    public static int turno;
+    public static float tprecio;
+
+
+}
